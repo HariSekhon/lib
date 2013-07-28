cedeb2570a454baf2bf2faac35a6234c29e1b007
Hari Sekhon <harisekhon@gmail.com>
2013-07-28 22:58:02 +0100

    updated HariSekhon/ZooKeeper.pm

--- HariSekhon/ZooKeeper.pm
+++ HariSekhon/ZooKeeper.pm
@@ -11,7 +11,7 @@
 
 package HariSekhon::ZooKeeper;
 
-$VERSION = "0.1";
+$VERSION = "0.2";
 
 use strict;
 use warnings;
@@ -35,6 +35,7 @@ our @EXPORT = ( qw (
 );
 our @EXPORT_OK = ( @EXPORT );
 
+# ZooKeeper Client Port
 our $ZK_DEFAULT_PORT = 2181;
 our $zk_port         = $ZK_DEFAULT_PORT;
 our @zk_valid_states = qw/leader follower standalone/;
@@ -42,12 +43,12 @@ our @zk_valid_states = qw/leader follower standalone/;
 our $zk_conn;
 # TODO: ZooKeeper closes connection after 1 cmd, see if I can work around this, as having to use several TCP connections is inefficient
 sub zoo_cmd {
-    vlog3 "connecting to $host:$port";
+    vlog3 "connecting to $host:$zk_port";
     $zk_conn = IO::Socket::INET->new (
                                         Proto    => "tcp",
                                         PeerAddr => $host,
-                                        PeerPort => $port,
-                                     ) or quit "CRITICAL", "Failed to connect to '$host:$port': $!";
+                                        PeerPort => $zk_port,
+                                     ) or quit "CRITICAL", "Failed to connect to '$host:$zk_port': $!";
     vlog3 "OK connected";
     my $cmd = defined($_[0]) ? $_[0] : code_error "no cmd arg defined for zoo_cmd()";
     vlog3 "sending request: '$cmd'";
