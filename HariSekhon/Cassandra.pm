6bd730b3c015e24a62fee8576e341f1314870148
Hari Sekhon <harisekhon@gmail.com>
2013-12-29 15:02:08 +0000

    updated HariSekhon/Cassandra.pm

--- HariSekhon/Cassandra.pm
+++ HariSekhon/Cassandra.pm
@@ -43,7 +43,7 @@ our %DEFAULT_CASSANDRA_PORT = (
 env_creds("CASSANDRA");
 
 our %cassandra_options = (
-    "H|host=s"         => [ \$main::host,         "Cassandra node to connect to" ],
+    "H|host=s"         => [ \$main::host,         "Cassandra node         (\$CASSANDRA_HOST, \$HOST)" ],
 );
 
 sub set_cassandra_port($){
@@ -52,9 +52,9 @@ sub set_cassandra_port($){
     defined($DEFAULT_CASSANDRA_PORT{$type}) or code_error "'$type' cassandra port not defined in HariSekhon::Cassandra::DEFAULT_CASSANDRA_PORT hash, passed in wrong type as arg?";
     %cassandra_options = (
         %cassandra_options,
-        "P|port=s"         => [ \$main::port,         sprintf("Cassandra %s port to connect to (default: %d)", $type, $DEFAULT_CASSANDRA_PORT{$type}) ],
-        "u|user=s"         => [ \$main::user,         sprintf("Cassandra %s User (optional, \$CASSANDRA_USER)", $type) ],
-        "p|password=s"     => [ \$main::password,     sprintf("Cassandra %s Password (optional, \$CASSANDRA_PASSWORD)", $type) ],
+        "P|port=s"         => [ \$main::port,         sprintf("Cassandra %s port     (default: %d, \$CASSANDRA_PORT, \$PORT)", $type, $DEFAULT_CASSANDRA_PORT{$type}) ],
+        "u|user=s"         => [ \$main::user,         sprintf("Cassandra %s user     (optional, \$CASSANDRA_USERNAME, \$CASSANDRA_USER, \$USERNAME, \$USER)", $type) ],
+        "p|password=s"     => [ \$main::password,     sprintf("Cassandra %s password (optional, \$CASSANDRA_PASSWORD, \$PASSWORD)", $type) ],
     );
     return $DEFAULT_CASSANDRA_PORT{$type};
 }
