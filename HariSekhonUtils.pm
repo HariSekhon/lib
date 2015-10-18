8b820bf097bad2b5610db89e1c2077b0093e7f90
Hari Sekhon <harisekhon@gmail.com>
2015-10-18 09:19:47 +0100

    changed Net::SSL to be a runtime import to better fit the conditional and not require the module be installed if it's not used

--- HariSekhonUtils.pm
+++ HariSekhonUtils.pm
@@ -66,10 +66,11 @@ use Term::ReadKey;
 use Time::Local;
 # Workaround for IO::Socket::SSL bug not respecting disabling verifying self-signed certs
 if( -f dirname(__FILE__) . "/.use_net_ssl" ){
-    use Net::SSL;
+    require Net::SSL;
+    import Net::SSL;
 }
 
-our $VERSION = "1.16.4";
+our $VERSION = "1.16.5";
 
 #BEGIN {
 # May want to refactor this so reserving ISA, update: 5.8.3 onwards
