d5b24d64c4002c680d69ce5dfc634896b5959a3d
Hari Sekhon <harisekhon@gmail.com>
2019-03-14 10:28:19 +0000

    allowed dots in usernames

--- HariSekhonUtils.pm
+++ HariSekhonUtils.pm
@@ -80,7 +80,7 @@ if( -f dirname(__FILE__) . "/.use_net_ssl" ){
     import Net::SSL;
 }
 
-our $VERSION = "1.19.3";
+our $VERSION = "1.19.4";
 
 #BEGIN {
 # May want to refactor this so reserving ISA, update: 5.8.3 onwards
@@ -709,7 +709,7 @@ our $host_regex         = "\\b(?:$hostname_regex|$ip_regex)\\b";
 our $process_name_regex = '\s*[\w_\.\/\<\>-][\w\s_\.\/\<\>-]*';
 our $url_path_suffix_regex = '/(?:[\w.,:\/%&?!#=*|\[\]~+-]+)?';
 our $url_regex          = '\b(?i:https?://' . $host_regex . '(?::\d{1,5})?(?:' . $url_path_suffix_regex . ')?)';
-our $user_regex         = '\b[A-Za-z0-9][A-Za-z0-9_-]*[A-Za-z0-9]\b';
+our $user_regex         = '\b[A-Za-z0-9][A-Za-z0-9\._-]*[A-Za-z0-9]\b';
 our $column_regex       = '\b[\w\:]+\b';
 our $ldap_dn_regex      = '\b\w+=[\w\s]+(?:,\w+=[\w\s]+)*\b';
 our $krb5_principal_regex = "$user_regex(?:\/$hostname_regex)?(?:\@$domain_regex)?";
